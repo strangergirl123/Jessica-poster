ae1e8027d36668e6e6848d96d0a8a64e7bb0a815
Jessica Lu <jl10698@nyu.edu>
2020-03-07 22:59:11 -0500

    Update sketch.js
    top of box and background

--- sketch.js
+++ sketch.js
@@ -1,5 +1,23 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    fill("black");
-    rect(0, 0, width, height);
+    
+}
+
+
+function draw() {
+  background('#2267c7');
+  translate(width/2, height/2);
+  fill('white')
+    rect(-250, -330, 450, 650);
+    fill('yellow');
+    noStroke();
+    quad(-30, -280, -250, -180, -30, -80, 200, -180);
+    fill('#e81577');
+    quad( -250, -330, -30, -330,-30, -280, -250, -180,);
+    quad(200, -330, -30, -330,-30, -280, 200, -180);
+}
+
+function stripes()
+{
+    
 }
