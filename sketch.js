a24371930919f84633f2235f80fb157868215fe4
Jessica Lu <jl10698@nyu.edu>
2020-03-07 23:25:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,9 +15,23 @@ function draw() {
     fill('#e81577');
     quad( -250, -330, -30, -330,-30, -280, -250, -180,);
     quad(200, -330, -30, -330,-30, -280, 200, -180);
+    fill('orange');
+    stripes();
+    translate(0,60);
+    fill('#e06416')
+    stripes();
+    translate(0,60);
+    fill('#f246a5');
+    stripes();
+    translate(0,60);
+    fill('#d61344');
+    stripes();
+    fill(10,40);
+    quad(-30,-260,-30,-20,200,-120,200,-360);
 }
 
 function stripes()
 {
-    
+    quad( -250, -120, -30, -20,-30, -50, -250, -150);
+    quad(200, -120, -30, -20,-30, -50, 200, -150); 
 }
